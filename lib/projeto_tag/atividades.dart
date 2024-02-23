89cc73580d2a786e0049ffebb8c657fedc60b9d2
Mateus Back <mateusfback14@gmail.com>
2024-02-22 22:10:05 -0300

    Add atividade 03

--- lib/projeto_tag/atividades.dart
+++ lib/projeto_tag/atividades.dart
@@ -1,13 +1,19 @@
 void main() {
   String projeto = 'Tag';
-  String tecnlogia = 'C#';
+  String tecnologia = 'C#';
   String gestorResponsavel = 'Gestor não nomeado';
   bool isprojetoAtivo = true;
   int quantidadeDevs = 32;
 
+  var projeto2 = 'Tag';
+  var tecnologia2 = 'Java';
+  var gestorResponsavel2 = 'Gestor não nomeado';
+  var isprojetoAtivo2 = true;
+  var quantidadeDevs2 = 32;
+
   //Interpolação
   print(
-      'O projeto $projeto, gerido por $gestorResponsavel, utiliza a tecnlogia $tecnlogia e tem $quantidadeDevs devs alocados');
+      'O projeto $projeto, gerido por $gestorResponsavel, utiliza a tecnologia $tecnologia e tem $quantidadeDevs devs alocados');
 
   //Concatenação
   // ignore: prefer_interpolation_to_compose_strings
@@ -15,13 +21,13 @@ void main() {
       projeto +
       ', gerido por ' +
       gestorResponsavel +
-      ', utiliza a tecnlogia ' +
-      tecnlogia +
+      ', utiliza a tecnologia ' +
+      tecnologia +
       ' e tem ' +
       quantidadeDevs.toString() +
       ' devs alocados');
 
   //Interpolação com processamento
   print(
-      'O projeto $projeto ${isprojetoAtivo ? 'está ativa' : 'está desativada'}, gerido por $gestorResponsavel, utiliza a tecnlogia $tecnlogia e $quantidadeDevs devs alocados');
+      'O projeto $projeto ${isprojetoAtivo ? 'está ativa' : 'está desativada'}, gerido por $gestorResponsavel, utiliza a tecnologia $tecnologia e $quantidadeDevs devs alocados');
 }
